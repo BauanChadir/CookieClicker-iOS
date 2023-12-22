4d3192fee48338ae82da353099d7dd0605eae60c
BauanChadir <121721172+BauanChadir@users.noreply.github.com>
2023-12-22 17:46:46 +0100

    Add files via upload

---
+++ CookieClicker/CookieClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieClickerApp.swift
+//  CookieClicker
+//
+//  Created by Bauan Chadir on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CookieClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
